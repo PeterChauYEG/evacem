ed17a2c980315098963c7131c491c518e9b92793
PeterChauYEG <peter.chau.esq@gmail.com>
2017-11-11 20:33:46 -0700

    deesktop media query complete

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,8 +7,6 @@ const Footer = () => (
   <div className='footer-container'>
     <div className='footer-content'>
       <p className='footer-text'>7302 118 Street NW</p>
-      <p className='footer-text'>Edmonton, AB</p>
-      <p className='footer-text'>T6G 1S4</p>
     </div>
   </div>
 )
--- src/components/hero.js
+++ src/components/hero.js
@@ -3,18 +3,18 @@ import React from 'react'
 // import css
 import '../index.css'
 
-// media
-import heroMobile from '../media/hero-mobile.png'
-
 const Hero = () => (
   <div className='hero'>
     <div className='hero-content'>
       <h1 className='hero-title'>Edmonton Vietnamese</h1>
       <h1 className='hero-title'>Alliance Church</h1>
-      <h2 className='hero-subtitle'>English Ministries</h2>
+      <div className='hero-sub'>
+        <h2 className='hero-subtitle'>English Ministries</h2>
+        <div className='hero-decoration' />
+      </div>
     </div>
     <div className='hero-image'>
-      <img src={heroMobile} alt='Edmonton Vietnamese Alliance Church' />
+      {/* <img src={heroMobile} alt='Edmonton Vietnamese Alliance Church' /> */}
     </div>
   </div>
 )
