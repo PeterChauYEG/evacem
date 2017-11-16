9eb428375897f16f78c0c3852dc0835dac6c0a96
PeterChauYEG <peter.chau.esq@gmail.com>
2017-11-15 21:20:24 -0700

    got the mobile and tablet for about done. need to fix desktop

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import React from 'react'
 import '../index.css'
 
 // Components
+import About from '../components/about'
 import Footer from '../components/footer'
 import Hero from '../components/hero'
 
 const App = () => (
   <div className='App'>
     <Hero />
+    <About />
     <Footer />
   </div>
 )
---
+++ src/components/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// import css
+import '../index.css'
+
+const About = () => (
+  <div className='about'>
+    <div className='about-decoration' />
+    <div className='about-container'>
+      <div className='about-content'>
+        <h1>Mission</h1>
+        <p>To develop relationships that demonstrate the love of God to all people and lead them to become devoted followers of Christ.</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default About
