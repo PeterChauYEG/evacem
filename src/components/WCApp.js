b96532f8204400385935fc07c245d382a5d5542b
PeterChauYEG <peter.chau.esq@gmail.com>
2017-11-30 21:16:26 -0700

    added verse, footer, and media queries

--- src/components/WCApp.js
+++ src/components/WCApp.js
@@ -4,11 +4,15 @@ import React from 'react'
 import '../index.css'
 
 // Components
+import Footer from '../components/footer'
 import WCHero from '../components/wchero'
+import WCVerse from '../components/wcverse'
 
 const WCApp = () => (
   <div className='WCApp'>
     <WCHero />
+    <WCVerse />
+    <Footer />
   </div>
 )
 
