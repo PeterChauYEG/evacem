383a97f9a56d7c6fc859ad00f73404e343292a1d
PeterChauYEG <peter.chau.esq@gmail.com>
2017-12-09 11:57:10 -0700

    updated bio

--- src/components/wcspeaker.js
+++ src/components/wcspeaker.js
@@ -27,7 +27,7 @@ const WCSpeaker = () => (
 
         <div className='wc-faq-body'>
           <p>
-            She is currently married to her husband Doug, and with their cat and their dog in a 600 square foot living space... times can be a little crazy! She loves to study and the more she hears about the more she wants to discover, Jen is currently pursuing her Masters in Theological Studies. Ultimately she is driven by a love for God and a deep passion to share that love with all she can. Jen loves to challenge men & women to find their authentic voice, a zest for life and their confidence in Christ. To encourage
+            She is currently married to her husband Doug, and with their cat and their dog in a 600 square foot living space... times can be a little crazy! She loves to study and the more she hears about the more she wants to discover, Jen is currently pursuing her Masters in Theological Studies. Ultimately she is driven by a love for God and a deep passion to share that love with all she can. Jen loves to challenge men & women to find their authentic voice, a zest for life and their confidence in Christ. To encourage bold living, breaking through comfort zones and learning to honor God with everything that they do.
           </p>
         </div>
       </div>
